8813fa3b22cda0585ba9cdebab4a485bf70be845
fengzj <fengzj@daojia-inc.com>
2019-01-22 22:17:06 +0800

    test

---
+++ src/main/java/com/centuryinfo/restaurantinfo/RestaurantinfoApplication.java
@@ -0,0 +1,20 @@
+package com.centuryinfo.restaurantinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RestaurantinfoApplication  extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantinfoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
+}
+
